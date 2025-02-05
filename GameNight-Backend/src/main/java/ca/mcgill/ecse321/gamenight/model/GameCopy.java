df34c39bb9c833969679fdcec92939a22f08b8dc
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-02-04 20:22:18 -0500

    adding getters and setters

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/GameCopy.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/GameCopy.java
@@ -2,4 +2,30 @@ package ca.mcgill.ecse321.gamenight.model;
 
 public class GameCopy {
 
+    private String description;
+    private Game game;
+    private GameOwner gameOwner;
+
+    public GameCopy(String description, Game game, GameOwner gameOwner) {
+        this.description = description;
+        this.game = game;
+        this.gameOwner = gameOwner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String setDescription(String newDescription) {
+        description = newDescription;
+        return description;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public GameOwner getOwner() {
+        return gameOwner;
+    }
 }
