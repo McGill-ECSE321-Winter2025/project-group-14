7a368c39b7899b3c54fa955b4737168cff430597
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-02-10 16:30:22 -0500

    Adding query method for highest rated games. Related to #13

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/repo/GameRepository.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/repo/GameRepository.java
@@ -1,9 +1,13 @@
 package ca.mcgill.ecse321.gamenight.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.gamenight.model.Game;
 
 public interface GameRepository extends CrudRepository<Game, String>{
-
+    @Query("SELECT r.game FROM GameReview r GROUP BY r.game ORDER BY AVG(r.rating) DESC LIMIT 3")
+    List<Game> findTheThreeHighestRatedGames();
 }
