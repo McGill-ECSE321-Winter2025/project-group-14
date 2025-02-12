25e082c6da790d9fc4885087a2c21e97705ca903
KassemYassine <kmy05@mail.aub.edu>
2025-02-12 14:54:21 -0500

    Adding persistence test layer. Related to #11

---
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/EventRepositoryTest.java
@@ -0,0 +1,38 @@
+package ca.mcgill.ecse321.gamenight.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.gamenight.model.Event;
+
+@SpringBootTest
+public class EventRepositoryTest {
+
+    @Autowired
+    private EventRepository eventRepo;
+
+    @AfterEach
+    public void clearDatabase() {
+        eventRepo.deleteAll();
+    }
+
+    @Test
+    public void testCreateAndRetrieveEvent() {
+        Event event = new Event("Game Night", "Board games evening", new Date(), new Date());
+        event = eventRepo.save(event);
+
+        Optional<Event> fetchedEvent = eventRepo.findById(event.getId());
+
+        assertNotNull(fetchedEvent.get());
+        assertEquals(event.getName(), fetchedEvent.get().getName());
+        assertEquals(event.getDescription(), fetchedEvent.get().getDescription());
+    }
+}
