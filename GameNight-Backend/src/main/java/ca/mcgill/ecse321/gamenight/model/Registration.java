897fc667d49e910c5ff59c53545dd3d525e48b17
dtuXu <ayhamnassar1@gmail.com>
2025-02-11 22:31:21 -0500

    Registeration domain

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Registration.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Registration.java
@@ -2,4 +2,39 @@ package ca.mcgill.ecse321.gamenight.model;
 
 public class Registration {
 
+    private int id;
+    private Event event;
+    private Player player;
+
+    public Registration() {
+    }
+
+    public Registration(Event event, Player player) {
+        this.event = event;
+        this.player = player;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
