a35ad14a28275848a569d0664ce1fbe7bc55a085
KassemYassine <kmy05@mail.aub.edu>
2025-02-11 23:07:56 -0500

    domain Event class

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Event.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Event.java
@@ -1,15 +1,49 @@
 package ca.mcgill.ecse321.gamenight.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.util.Date;
 
-@Entity
 public class Event {
+    private String name;
+    private String description;
+    private Date startTime;
+    private Date endTime;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    public Event(String name, String description, Date startTime, Date endTime) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 }
