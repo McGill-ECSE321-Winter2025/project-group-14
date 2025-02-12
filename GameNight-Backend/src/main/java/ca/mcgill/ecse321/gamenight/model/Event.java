af34009ec1338be1dbc4c5a1a84b20de3839131d
KassemYassine <kmy05@mail.aub.edu>
2025-02-11 23:07:56 -0500

    domain Event class

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Event.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Event.java
@@ -1,5 +1,49 @@
 package ca.mcgill.ecse321.gamenight.model;
 
-public class Event {
+import java.util.Date;
 
+public class Event {
+    private String name;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+
+    public Event(String name, String description, Date startTime, Date endTime) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 }
