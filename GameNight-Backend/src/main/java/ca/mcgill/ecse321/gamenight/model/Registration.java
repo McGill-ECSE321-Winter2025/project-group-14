7fa04c52ec94ff7c8bb50209660b5aa3bb144d86
dtuXu <ayhamnassar1@gmail.com>
2025-02-11 22:46:33 -0500

    Persistence for registration

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Registration.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/Registration.java
@@ -1,9 +1,20 @@
 package ca.mcgill.ecse321.gamenight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity     
 public class Registration {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @ManyToOne
     private Event event;
+    @ManyToOne
     private Player player;
 
     public Registration() {
