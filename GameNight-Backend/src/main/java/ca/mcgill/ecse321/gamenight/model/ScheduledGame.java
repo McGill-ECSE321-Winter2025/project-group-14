439fbfa6489fd1b2dea0d0a6da4f4a4e7fe339f8
dtuXu <ayhamnassar1@gmail.com>
2025-02-11 22:53:34 -0500

    scheduled game domain

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/ScheduledGame.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/model/ScheduledGame.java
@@ -2,4 +2,39 @@ package ca.mcgill.ecse321.gamenight.model;
 
 public class ScheduledGame {
 
+    private int id;
+    private Game game;
+    private Event event;
+
+    public ScheduledGame() {
+    }
+
+    public ScheduledGame(Game game, Event event) {
+        this.game = game;
+        this.event = event;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
