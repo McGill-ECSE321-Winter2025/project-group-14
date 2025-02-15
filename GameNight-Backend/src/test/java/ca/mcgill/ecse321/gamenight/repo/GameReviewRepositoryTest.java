7bceab5f8189ff3f430057d1d2518ccf43f6dfa0
Mathis Bélanger <mathisbelangerr@gmail.com>
2025-02-15 01:08:34 -0500

    refactor game review test

--- GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/GameReviewRepositoryTest.java
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/GameReviewRepositoryTest.java
@@ -1,18 +1,12 @@
 package ca.mcgill.ecse321.gamenight.repo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import ca.mcgill.ecse321.gamenight.model.Game;
-import ca.mcgill.ecse321.gamenight.model.GameReview;
-import ca.mcgill.ecse321.gamenight.model.Person;
-import ca.mcgill.ecse321.gamenight.model.Player;
+import ca.mcgill.ecse321.gamenight.model.*;
 
 @SpringBootTest
 public class GameReviewRepositoryTest {
@@ -29,6 +23,25 @@ public class GameReviewRepositoryTest {
     @Autowired
     private PlayerRepository playerRepo;
 
+    private Person person;
+    private Player player;
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        clearDatabase();
+
+        person = new Person("aaaaaa@gmail.com", "aaaaa", "Bertrand");
+        personRepo.save(person);
+
+        player = new Player();
+        player.setPerson(person);
+        playerRepo.save(player);
+
+        game = new Game("Batman", "A Batman game");
+        gameRepo.save(game);
+    }
+
     @AfterEach
     public void clearDatabase() {
         gameReviewRepo.deleteAll();
@@ -39,27 +52,11 @@ public class GameReviewRepositoryTest {
 
     @Test
     public void testCreateAndReadGameReview() {
-        // Create a Person
-        Person person = new Person("aaaaaa@gmail.com", "aaaaa", "Bertrand");
-        personRepo.save(person);
-
-        // Create Player
-        Player player = new Player();
-        player.setPerson(person);
-        playerRepo.save(player);
-
-        // Create a Game
-        Game game = new Game("Batman", "A Batman game");
-        gameRepo.save(game);
-
-        // Create a GameReview
         GameReview gameReview = new GameReview(5, "Great game!", player, game);
         gameReviewRepo.save(gameReview);
 
-        // Read the GameReview from the database
         GameReview retrievedGameReview = gameReviewRepo.findById(gameReview.getId()).orElse(null);
 
-        // Assertions
         assertNotNull(retrievedGameReview);
         assertEquals(gameReview.getRating(), retrievedGameReview.getRating());
         assertEquals(gameReview.getComment(), retrievedGameReview.getComment());
@@ -70,30 +67,13 @@ public class GameReviewRepositoryTest {
 
     @Test
     public void testDeleteGameReview() {
-        // Create a Person
-        Person person = new Person("aaaaaa@gmail.com", "aaaaa", "Bertrand");
-        personRepo.save(person);
-
-        // Create Player
-        Player player = new Player();
-        player.setPerson(person);
-        playerRepo.save(player);
-
-        // Create a Game
-        Game game = new Game("Batman", "A Batman game");
-        gameRepo.save(game);
-
-        // Create a GameReview
         GameReview gameReview = new GameReview(5, "Great game!", player, game);
         gameReviewRepo.save(gameReview);
 
-        // Delete the GameReview
         gameReviewRepo.delete(gameReview);
 
-        // Try to retrieve the deleted GameReview
         GameReview deletedGameReview = gameReviewRepo.findById(gameReview.getId()).orElse(null);
 
-        // Assertions
         assertNull(deletedGameReview);
     }
 }
