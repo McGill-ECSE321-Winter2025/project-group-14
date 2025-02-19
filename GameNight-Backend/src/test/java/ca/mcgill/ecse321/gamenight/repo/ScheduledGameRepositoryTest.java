08b81bfc482425e93feae51042ee3eb2495cf036
Deniz Kuru <deniz.kuru@mail.mcgill.ca>
2025-02-18 19:11:15 -0500

    Changing ScheduledGameRepositoryTest to fit with new composite key queries. Related to #3

--- GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/ScheduledGameRepositoryTest.java
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/ScheduledGameRepositoryTest.java
@@ -1,9 +1,9 @@
 package ca.mcgill.ecse321.gamenight.repo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Optional;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.gamenight.model.ScheduledGame;
+import ca.mcgill.ecse321.gamenight.model.ScheduledGame.Key;
 import ca.mcgill.ecse321.gamenight.model.Game;
 import ca.mcgill.ecse321.gamenight.model.Event;
 
@@ -38,16 +39,63 @@ public class ScheduledGameRepositoryTest {
         Game game = new Game("Catan", "A strategy board game");
         game = gameRepo.save(game);
 
-        Event event = new Event("Game Night", "An evening of board games", null, null);
+        Date startTime = new Date();
+        Date endTime = new Date(startTime.getTime() + 7200000); // 2 hours later
+        Event event = new Event("Game Night", "An evening of board games", startTime, endTime);
         event = eventRepo.save(event);
 
-        ScheduledGame scheduledGame = new ScheduledGame(game, event);
+        ScheduledGame.Key key = new ScheduledGame.Key(game, event);
+        ScheduledGame scheduledGame = new ScheduledGame(key);
         scheduledGame = scheduledGameRepo.save(scheduledGame);
 
-        Optional<ScheduledGame> fetchedScheduledGame = scheduledGameRepo.findById(scheduledGame.getId());
+        ScheduledGame fetchedScheduledGame = scheduledGameRepo.findByKey(key);
 
-        assertNotNull(fetchedScheduledGame.get());
-        assertEquals(game.getName(), fetchedScheduledGame.get().getGame().getName());
-        assertEquals(event.getName(), fetchedScheduledGame.get().getEvent().getName());
+        assertNotNull(fetchedScheduledGame, "Scheduled game should be present in repository");
+        assertEquals(game.getName(), fetchedScheduledGame.getKey().getGame().getName());
+        assertEquals(event.getName(), fetchedScheduledGame.getKey().getEvent().getName());
+        assertEquals(startTime, fetchedScheduledGame.getKey().getEvent().getStartTime());
+        assertEquals(endTime, fetchedScheduledGame.getKey().getEvent().getEndTime());
+    }
+
+    @Test
+    public void testFindByGame() {
+        Game game = new Game("Catan", "Board game");
+        game = gameRepo.save(game);
+
+        Date startTime = new Date();
+        Event event1 = new Event("Game Night One", "Board games night", startTime, new Date(startTime.getTime() + 3600000));
+        Event event2 = new Event("Game Night Two", "More board games", startTime, new Date(startTime.getTime() + 3600000));
+        event1 = eventRepo.save(event1);
+        event2 = eventRepo.save(event2);
+
+        ScheduledGame sg1 = new ScheduledGame(new Key(game, event1));
+        ScheduledGame sg2 = new ScheduledGame(new Key(game, event2));
+        scheduledGameRepo.save(sg1);
+        scheduledGameRepo.save(sg2);
+
+        List<ScheduledGame> gamesList = scheduledGameRepo.findByKey_GameId(game.getId());
+        assertNotNull(gamesList, "Games list should not be null");
+        assertEquals(2, gamesList.size(), "There should be two scheduled games for the game");
+    }
+
+    @Test
+    public void testFindByEvent() {
+        Game game1 = new Game("Catan", "Strategy game");
+        Game game2 = new Game("Monopoly", "Economic game");
+        game1 = gameRepo.save(game1);
+        game2 = gameRepo.save(game2);
+
+        Date startTime = new Date();
+        Event event = new Event("Board Game Marathon", "A day of games", startTime, new Date(startTime.getTime() + 7200000));
+        event = eventRepo.save(event);
+
+        ScheduledGame sg1 = new ScheduledGame(new Key(game1, event));
+        ScheduledGame sg2 = new ScheduledGame(new Key(game2, event));
+        scheduledGameRepo.save(sg1);
+        scheduledGameRepo.save(sg2);
+
+        List<ScheduledGame> eventsList = scheduledGameRepo.findByKey_EventId(event.getId());
+        assertNotNull(eventsList, "Events list should not be null");
+        assertEquals(2, eventsList.size(), "There should be two scheduled games for the event");
     }
 }
