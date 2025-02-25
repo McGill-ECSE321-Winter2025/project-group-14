1c7662b26312698aee95021e27bfff81f4850fdb
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-02-24 19:42:16 -0500

    Added mocking and more unit testsfor GameManagementService. Related to #35

--- GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/service/GameManagementServiceTest.java
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/service/GameManagementServiceTest.java
@@ -2,16 +2,25 @@ package ca.mcgill.ecse321.gamenight.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Optional;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.gamenight.model.Game;
+import ca.mcgill.ecse321.gamenight.model.GameCopy;
 import ca.mcgill.ecse321.gamenight.model.GameOwner;
 import ca.mcgill.ecse321.gamenight.model.Person;
 import ca.mcgill.ecse321.gamenight.repo.GameCopyRepository;
@@ -22,55 +31,133 @@ import ca.mcgill.ecse321.gamenight.repo.PersonRepository;
 @SpringBootTest
 public class GameManagementServiceTest {
 
-    @Autowired
+    @Mock
     GameRepository gameRepository;
 
-    @Autowired
+    @Mock
     GameCopyRepository gameCopyRepository;
 
-    @Autowired
-    GameManagementService gameManagementService;
+    @Mock
+    GameOwnerRepository gameOwnerRepository;
 
-    @Autowired
+    @Mock
     PersonRepository personRepository;
 
-    @Autowired
-    GameOwnerRepository gameOwnerRepository;
+    @InjectMocks
+    GameManagementService gameManagementService;
 
     private GameOwner owner;
 
-    @AfterEach
-    public void clearDatabase() {
-        gameCopyRepository.deleteAll();
-        gameRepository.deleteAll();
-        gameOwnerRepository.deleteAll();
-        personRepository.deleteAll();
-    }
 
     @BeforeEach
     public  void setup() {
         Person p = new Person("hello@outlook.com", "pswd", "Alex");
         owner = new GameOwner(p);
+        when(gameOwnerRepository.findById(owner.getId())).thenReturn(Optional.ofNullable(owner));
     }
 
     @Test
     public void createValidGameTest() {
         String name = "Uno";
         String description = "A card game";
+        Game game = new Game(name, description);
+        when(gameRepository.save(any(Game.class))).thenReturn(game);
 
         Game createdGame = gameManagementService.createGame(name, description);
 
-        // check that the instance returned has the correct information
         assertNotNull(createdGame);
         assertEquals(name, createdGame.getName());
         assertEquals(description, createdGame.getDescription());
+    }
 
-        // check that the game was successfully added to the database
-        Optional<Game> game = gameRepository.findById(createdGame.getId());
-        assertNotNull(game);
-        Game gameInDB = game.get();
-        assertEquals(createdGame.getId(), gameInDB.getId());
-        assertEquals(createdGame.getName(), gameInDB.getName());
-        assertEquals(createdGame.getDescription(), gameInDB.getDescription());
+    @Test
+    public void failToCreateGameWithNoName() {
+        Exception e = assertThrows(IllegalArgumentException.class, () ->
+            gameManagementService.createGame(null, "A  card game"));
+        assertEquals("Game must have a name", e.getMessage());
+        assertEquals(0, gameRepository.count());
+    }
+
+    @Test
+    public void updateGameTest() {
+        Game game = new Game("Uno", "A card game");
+        when(gameRepository.findById(game.getId())).thenReturn(Optional.ofNullable(game));
+
+        Game savedGame = gameManagementService.updateGame(game.getId(), "Monopoly", "A board game");
+
+        assertNotNull(savedGame);
+        assertEquals(game.getId(), savedGame.getId());
+        assertEquals("Monopoly", savedGame.getName());
+        assertEquals("A board game", savedGame.getDescription());
+    }
+
+    @Test
+    public void deleteGameTest() {
+        Game game = new Game("Uno", "A card game");
+        when(gameRepository.findById(game.getId())).thenReturn(Optional.ofNullable(game));
+
+        gameManagementService.deleteGame(game.getId());
+
+        verify(gameRepository, times(1)).delete(game);
+    }
+
+    @Test
+    public void findExistsingGameByIdTest() {
+        Game game = new Game("Uno", "A card game");
+        when(gameRepository.findById(game.getId())).thenReturn(Optional.ofNullable(game));
+
+        Game foundGame = gameManagementService.findGameById(game.getId());
+
+        assertEquals(game.getId(), foundGame.getId());
+        assertEquals(game.getName(), foundGame.getName());
+        assertEquals(game.getDescription(), foundGame.getDescription());
+    }
+
+    @Test
+    public void tryToFindGameNotInDBTest() {
+        int id = 5;
+        when(gameRepository.findById(id)).thenReturn(Optional.ofNullable(null));
+
+        Exception e = assertThrows(IllegalArgumentException.class, () ->
+            gameManagementService.findGameById(id));
+        assertEquals("There is no game with ID " + id , e.getMessage());
+    }
+
+    @Test
+    public void findAllGamesTest() {
+        Game game1 = new Game("Uno", "A card game");
+        Game game2 = new Game("Monopoly", "A board game");
+        ArrayList<Game> gamesList = new ArrayList<>();
+        gamesList.add(game1);
+        gamesList.add(game2);
+        when(gameRepository.findAll()).thenReturn(gamesList);
+
+        Iterator<Game> games = gameManagementService.findAllGames().iterator();
+
+        Game game1FromDB = games.next();
+        assertNotNull(game1FromDB);
+        assertEquals(game1.getId(), game1FromDB.getId());
+        assertEquals(game1.getName(), game1FromDB.getName());
+        assertEquals(game1.getDescription(), game1FromDB.getDescription());
+
+        Game game2FromDB = games.next();
+        assertNotNull(game2FromDB);
+        assertEquals(game2.getId(), game2FromDB.getId());
+        assertEquals(game2.getName(), game2FromDB.getName());
+        assertEquals(game2.getDescription(), game2FromDB.getDescription());
+    }
+
+    @Test
+    public void addGameCopyTest() {
+        Game game = new Game("Uno", "A card game");
+        when(gameRepository.findById(game.getId())).thenReturn(Optional.ofNullable(game));
+        GameCopy gameCopy = new GameCopy("Lost a card", game, owner);
+        when(gameCopyRepository.findById(anyInt())).thenReturn(Optional.ofNullable(gameCopy));
+
+        GameCopy g = gameManagementService.addGameCopy("Lost a card", game.getId(), owner.getId());
+
+        assertEquals(owner.getId(), g.getOwner().getId());
+        assertEquals(game.getId(), g.getGame().getId());
+        assertEquals("Lost a card", g.getDescription());
     }
 }
