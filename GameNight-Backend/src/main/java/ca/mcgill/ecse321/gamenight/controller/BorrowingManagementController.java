50dc4ab531f351e31de4e323ba64a6d61a8f90b0
ReinaEl-Hoz <rehz2004@gmail.com>
2025-03-01 16:00:39 -0500

    controller

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
@@ -1,13 +1,27 @@
 package ca.mcgill.ecse321.gamenight.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.gamenight.dto.BorrowingRequestRequestDto;
+import ca.mcgill.ecse321.gamenight.dto.BorrowingRequestResponseDto;
 import ca.mcgill.ecse321.gamenight.service.BorrowingManagementService;
 
 @RestController
 public class BorrowingManagementController {
     @Autowired
     BorrowingManagementService borrowingManagementService;
-    
+
+    @PostMapping
+    public BorrowingRequestResponseDto createBorrowingRequest(@RequestBody BorrowingRequestRequestDto borrowingRequest){
+            return null;  
+    }
+
+    @PutMapping
+    public BorrowingRequestResponseDto respondToBorrowingRequest(@RequestBody BorrowingRequestRequestDto borrowingRequest){
+            return null;  
+    }
 }
