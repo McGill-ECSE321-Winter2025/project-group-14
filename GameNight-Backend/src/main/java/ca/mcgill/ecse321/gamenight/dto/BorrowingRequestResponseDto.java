007fd0f99521821352d2f8f3a4d22b8aa573901b
hamzadaqa <abudaqahamza@gmail.com>
2025-03-01 15:47:01 -0500

    Implemented DTO

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/BorrowingRequestResponseDto.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/BorrowingRequestResponseDto.java
@@ -1,5 +1,60 @@
 package ca.mcgill.ecse321.gamenight.dto;
 
+import java.sql.Date;
+
+import ca.mcgill.ecse321.gamenight.model.BorrowingRequest;
+import ca.mcgill.ecse321.gamenight.model.BorrowingRequest.BorrowingRequestStatus;
+
+
 public class BorrowingRequestResponseDto {
+    private int id;
+    private Date sendTime;
+    private Date startTime;
+    private Date endTime;
+    private String gameName;
+    private String senderName;
+    private BorrowingRequestStatus status;
     
+    @SuppressWarnings("unused")
+    private BorrowingRequestResponseDto(){}
+
+    public BorrowingRequestResponseDto(BorrowingRequest request){
+        this.id=request.getId();
+        this.sendTime = request.getSendTime();
+        this.endTime = request.getEndTime();
+        this.startTime = request.getStartTime();
+        this.status=request.getStatus();
+        this.gameName = request.getGameCopy().getGame().getName();
+        this.senderName = request.getSender().getPerson().getName();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public String getGameName() { 
+        return gameName; 
+    }
+
+    public String getSenderName() { 
+        return senderName;
+    }
+
+    public BorrowingRequestStatus getStatus() {
+        return status;
+    }
+
 }
+
