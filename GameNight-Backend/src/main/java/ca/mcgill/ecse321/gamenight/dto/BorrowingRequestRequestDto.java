0784d3777b2535bac2158befd77ab48c85681667
ReinaEl-Hoz <rehz2004@gmail.com>
2025-03-01 16:31:50 -0500

    dto

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/BorrowingRequestRequestDto.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/BorrowingRequestRequestDto.java
@@ -1,5 +1,50 @@
 package ca.mcgill.ecse321.gamenight.dto;
 
+import java.sql.Date;
+
+import ca.mcgill.ecse321.gamenight.model.BorrowingRequest;
+import ca.mcgill.ecse321.gamenight.model.BorrowingRequest.BorrowingRequestStatus;
+
 public class BorrowingRequestRequestDto {
+     private int id;
+    private Date sendTime;
+    private Date startTime;
+    private Date endTime;
+    private String gameName;
+    private String senderName;
+    private BorrowingRequestStatus status;
     
+    @SuppressWarnings("unused")
+    private BorrowingRequestRequestDto(){
+    }
+    public BorrowingRequestRequestDto(BorrowingRequest request){
+        this.id=request.getId();
+        this.sendTime = request.getSendTime();
+        this.endTime = request.getEndTime();
+        this.startTime = request.getStartTime();
+        this.status=request.getStatus();
+        this.gameName = request.getGameCopy().getGame().getName();
+        this.senderName = request.getSender().getPerson().getName();
+    }
+    public int getId() {
+        return id;
+    }
+    public Date getSendTime() {
+        return sendTime;
+    }
+    public Date getStartTime() {
+        return startTime;
+    }
+    public Date getEndTime() {
+        return endTime;
+    }
+    public String getGameName() {
+        return gameName;
+    }
+    public String getSenderName() {
+        return senderName;
+    }
+    public BorrowingRequestStatus getStatus() {
+        return status;
+    }
 }
--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/service/BorrowingManagementService.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/service/BorrowingManagementService.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.gamenight.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.gamenight.model.BorrowingRequest;
