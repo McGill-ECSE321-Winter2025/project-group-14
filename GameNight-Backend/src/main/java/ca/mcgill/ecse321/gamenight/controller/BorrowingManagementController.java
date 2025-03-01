5034d38ed397d7b7e5326cd90648708292c79bb8
ReinaEl-Hoz <rehz2004@gmail.com>
2025-03-01 14:14:34 -0500

    Dto folder

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
@@ -1,5 +1,13 @@
 package ca.mcgill.ecse321.gamenight.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.gamenight.service.BorrowingManagementService;
+
+@RestController
 public class BorrowingManagementController {
+    @Autowired
+    BorrowingManagementService borrowingManagementService;
     
 }
---
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/BorrowingRequestRequestDto.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.gamenight.dto;
+
+public class BorrowingRequestRequestDto {
+    
+}
