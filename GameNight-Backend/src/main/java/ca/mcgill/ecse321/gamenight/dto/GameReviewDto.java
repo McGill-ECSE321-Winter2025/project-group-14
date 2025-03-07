d610c023fceb87147ac8c14a9367a43765b93bfd
Mathis Bélanger <mathisbelangerr@gmail.com>
2025-03-07 11:33:52 -0500

    added gamereview dto

---
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/GameReviewDto.java
@@ -0,0 +1,63 @@
+package ca.mcgill.ecse321.gamenight.dto;
+
+public class GameReviewDto {
+
+    private int reviewId;
+    private int rating;
+    private String comment;
+    private int reviewerId; // Reference to the reviewer (Player)
+    private int gameId; // Reference to the game (Game)
+
+    // Constructors
+    public GameReviewDto() {
+    }
+
+    public GameReviewDto(int reviewId, int rating, String comment, int reviewerId, int gameId) {
+        this.reviewId = reviewId;
+        this.rating = rating;
+        this.comment = comment;
+        this.reviewerId = reviewerId;
+        this.gameId = gameId;
+    }
+
+    // Getters and Setters
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getReviewerId() {
+        return reviewerId;
+    }
+
+    public void setReviewerId(int reviewerId) {
+        this.reviewerId = reviewerId;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+}
