b9c0a73379bcdcad4b6a6c4a44b6e97fae9d41c0
Mathis Bélanger <mathisbelangerr@gmail.com>
2025-03-08 11:50:16 -0500

    removed setter DTO

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/GameReviewDto.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/GameReviewDto.java
@@ -23,39 +23,19 @@ public class GameReviewDto {
         return reviewId;
     }
 
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
-    }
-
     public int getRating() {
         return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
     }
 
     public String getComment() {
         return comment;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
     public int getReviewerId() {
         return reviewerId;
     }
 
-    public void setReviewerId(int reviewerId) {
-        this.reviewerId = reviewerId;
-    }
-
     public int getGameId() {
         return gameId;
-    }
-
-    public void setGameId(int gameId) {
-        this.gameId = gameId;
     }
 }
