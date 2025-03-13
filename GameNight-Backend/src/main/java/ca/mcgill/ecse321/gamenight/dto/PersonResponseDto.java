2db679a2744c6bfcedc02aafcf820676c142a9ce
KassemYassine <kmy05@mail.aub.edu>
2025-03-13 04:10:52 -0400

    Added needed DTOs.Related to #43

---
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/dto/PersonResponseDto.java
@@ -0,0 +1,27 @@
+package ca.mcgill.ecse321.gamenight.dto;
+
+import ca.mcgill.ecse321.gamenight.model.Person;
+
+public class PersonResponseDto {
+    private int personId;
+    private String name;
+    private String email;
+
+    public PersonResponseDto(Person person) {
+        this.personId = person.getId();
+        this.name = person.getName();
+        this.email = person.getEmailAddress();
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
