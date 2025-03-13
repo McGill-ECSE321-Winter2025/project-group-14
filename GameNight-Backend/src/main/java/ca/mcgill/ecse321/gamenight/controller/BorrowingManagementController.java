eceb763729312c73265d7dd79b21a42d64c88471
ReinaEl-Hoz <rehz2004@gmail.com>
2025-03-13 13:05:05 -0400

    added more tests/fixed integration test. Related to #41

--- GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
+++ GameNight-Backend/src/main/java/ca/mcgill/ecse321/gamenight/controller/BorrowingManagementController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.gamenight.dto.BorrowingRequestRequestDto;
@@ -34,6 +36,7 @@ public class BorrowingManagementController {
         private GameCopyRepository gameCopyRepository;
         
         @PostMapping("")
+        @ResponseStatus(HttpStatus.CREATED)
         public BorrowingRequestResponseDto sendBorrowingRequest(@RequestBody BorrowingRequestRequestDto borrowingRequest) {
                 BorrowingRequest savedRequest = borrowingManagementService.sendBorrowingRequest(
                         borrowingRequest.getGameCopyId(),
--- GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/integration/BorrowingManagementIntegrationTests.java
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/integration/BorrowingManagementIntegrationTests.java
@@ -12,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.Date;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,17 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import ca.mcgill.ecse321.gamenight.dto.BorrowingRequestRequestDto;
 import ca.mcgill.ecse321.gamenight.dto.BorrowingRequestResponseDto;
+import ca.mcgill.ecse321.gamenight.model.Game;
+import ca.mcgill.ecse321.gamenight.model.GameCopy;
+import ca.mcgill.ecse321.gamenight.model.GameOwner;
+import ca.mcgill.ecse321.gamenight.model.Person;
+import ca.mcgill.ecse321.gamenight.model.Player;
+import ca.mcgill.ecse321.gamenight.repo.BorrowingRequestRepository;
+import ca.mcgill.ecse321.gamenight.repo.GameCopyRepository;
+import ca.mcgill.ecse321.gamenight.repo.GameOwnerRepository;
+import ca.mcgill.ecse321.gamenight.repo.GameRepository;
+import ca.mcgill.ecse321.gamenight.repo.PersonRepository;
+import ca.mcgill.ecse321.gamenight.repo.PlayerRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -29,16 +42,78 @@ public class BorrowingManagementIntegrationTests {
     @Autowired
     private TestRestTemplate client;
 
+    @Autowired
+    private GameCopyRepository gameCopyRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Autowired
+    private GameOwnerRepository gameOwnerRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private BorrowingRequestRepository borrowingRequestRepository;
+
+
+    private int validSenderId;
+    private int validGameCopyId;
+    private GameOwner createdGameOwner;
+    private static final String VALID_EMAIL1 = "johnash@gmail.com";
+    private static final String VALID_EMAIL2 = "sender@gmail.com";
+    private static final String VALID_PASSWORD1 = "1234RE";
+    private static final String VALID_NAME1 = "John Nashville";
+    private static final String VALID_GAMENAME = "Uno";
+    private static final String VALID_DESCRIPTION = "card game";
+    private static final String VALID_GAMECOPYDESCRIPTION =  "Good condition";
+    private static final String VALID_PASSWORD2 = "1234RE";
+    private static final String VALID_NAME2 = "jane doe";
+    
+
+
+
+    @BeforeAll
+    public void setup(){
+        gameCopyRepository.deleteAll();
+        gameRepository.deleteAll();
+        gameOwnerRepository.deleteAll();
+        personRepository.deleteAll();
+        playerRepository.deleteAll();
+
+        Person ownerPerson = personRepository.save(new Person(VALID_EMAIL1, VALID_PASSWORD1, VALID_NAME1));
+        createdGameOwner = gameOwnerRepository.save(new GameOwner(ownerPerson));
+        Game game = new Game(VALID_GAMENAME, VALID_DESCRIPTION);
+        game = gameRepository.save(game);
+        GameCopy gameCopy = new GameCopy(VALID_GAMECOPYDESCRIPTION, game, createdGameOwner);
+        gameCopy = gameCopyRepository.save(gameCopy);
+        validGameCopyId = gameCopy.getId();
+        Person senderPerson = personRepository.save(new Person(VALID_EMAIL2, VALID_PASSWORD2, VALID_NAME2));
+        Player sender = new Player(senderPerson);
+        sender = playerRepository.save(sender);
+        validSenderId = sender.getId();
+    }
+    @AfterAll
+    public void cleanup(){
+        borrowingRequestRepository.deleteAll();
+        gameCopyRepository.deleteAll();
+        playerRepository.deleteAll();
+        gameOwnerRepository.deleteAll();
+        gameRepository.deleteAll();
+        personRepository.deleteAll();
+    }
     @Test
     @Order(0)
     public void testsendValidBorrowingRequest() {
         
         Date startTime = Date.valueOf("2025-01-05");
         Date endTime = Date.valueOf("2025-01-10");
-        int senderId =1;
-        int gameCopyId = 5;
-        
-        BorrowingRequestRequestDto request = new BorrowingRequestRequestDto(startTime, endTime, senderId, gameCopyId);
+
+        BorrowingRequestRequestDto request = new BorrowingRequestRequestDto(startTime, endTime, validSenderId, validGameCopyId);
         ResponseEntity<BorrowingRequestResponseDto> response = 
                 client.postForEntity("/borrowingRequests", request, BorrowingRequestResponseDto.class);
 
@@ -48,5 +123,6 @@ public class BorrowingManagementIntegrationTests {
         assertNotNull(responseBody);
         assertNotNull(responseBody.getSendTime());
     }
+
 
 }
