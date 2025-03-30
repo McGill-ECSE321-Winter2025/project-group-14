51156410447ad573ff2d9fc7a131d0942c1f65b7
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-03-30 15:17:57 -0400

    Adding search bar for games page

--- gamenight-frontend/src/pages/GamePage.js
+++ gamenight-frontend/src/pages/GamePage.js
@@ -13,16 +13,36 @@ const GamePage = () => {
         .catch((error) => console.error("Error fetching games:", error));
     }, []);
 
+    const [query, setQuery] = useState("");
+    const filteredGames = games.filter((game) =>
+        game.name.toLowerCase().includes(query.toLowerCase())
+    );
+
 return (
     <div className="container">
         <Box>
             <h1 class="centered">Games</h1>
+
+            <div className="centered">
+                <input
+                    type="text"
+                    placeholder="Search for a game..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="search-input"
+                />
+            </div>
+
             <div className="game-list">
-                {games.map((game, index) => (
-                    <div key={index} className="fade-in-card">
-                        <Game key={game.id} id={game.id} title={game.name} rating={game.rating} image={picture} />
-                    </div>
-                ))}
+                {filteredGames.length > 0 ? (
+                    filteredGames.map((game, index) => (
+                        <div key={index} className="fade-in-card">
+                            <Game key={game.id} id={game.id} title={game.name} rating={game.rating} image={picture} />
+                        </div>
+                    ))
+                ):(
+                    <p>No results found</p>
+                )}
             </div>
         </Box>
     </div>
