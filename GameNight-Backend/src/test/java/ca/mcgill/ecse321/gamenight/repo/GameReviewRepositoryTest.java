8adc0229c5fbcb5a6b0cad868bd40f14651b1897
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-04-04 09:48:13 -0400

    Fixing some failing tests after changes to controller for frontend

--- GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/GameReviewRepositoryTest.java
+++ GameNight-Backend/src/test/java/ca/mcgill/ecse321/gamenight/repo/GameReviewRepositoryTest.java
@@ -109,13 +109,14 @@ public class GameReviewRepositoryTest {
         GameReview gameReview1 = new GameReview(5, "Great game!", player, game);
         gameReviewRepo.save(gameReview1);
 
-        GameReview gameReview2 = new GameReview(4, "Good game!", player, game);
-        gameReviewRepo.save(gameReview2);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        
+        GameReview gameReview2 = new GameReview(4, "Good game!", player, game);
+        gameReviewRepo.save(gameReview2);
 
         List<GameReview> reviews = gameReviewRepo.findByGameOrderByDatePostedDesc(game);
 
