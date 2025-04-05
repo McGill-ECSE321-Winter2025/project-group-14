efeeaafcfdcd86dbfe83b0441d0b0c376a187cc7
KassemYassine <kmy05@mail.aub.edu>
2025-04-05 19:00:41 -0400

    small detailling

--- gamenight-frontend/src/index.js
+++ gamenight-frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-// Now set the base URL after all the imports
+
 axios.defaults.baseURL = 'http://localhost:8080';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
