358d7b0ca2f1403550cb71f24a270672ea94a350
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-04-06 10:28:31 -0400

    About is leftmost on navbar

--- gamenight-frontend/src/components/navigation/Navbar.js
+++ gamenight-frontend/src/components/navigation/Navbar.js
@@ -16,9 +16,9 @@ function Navbar() {
         <nav className="navbar">
             <div className="nav-left">
                 {!user && <Link to="/">Home</Link>}
+                {user && <Link to="/about">About</Link>}
                 {user && <Link to="/games">Games</Link>}
                 {user && <Link to="/events">Events</Link>}
-                {user && <Link to="/about">About</Link>}
                 {user && isOwner && <Link to="/received-requests">Borrowing Requests</Link>}
                 
   
