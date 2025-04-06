cb0f0953b315ed4188a2522f0b4b59f09e199089
hamzadaqa <abudaqahamza@gmail.com>
2025-04-06 04:57:10 -0400

    made the cards of Incoming requests horizontally bigger. Related to issue #100

--- gamenight-frontend/src/components/navigation/Navbar.js
+++ gamenight-frontend/src/components/navigation/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
                 {user && <Link to="/games">Games</Link>}
                 {user && <Link to="/events">Events</Link>}
                 {user && <Link to="/about">About</Link>}
-                {user && isOwner && <Link to="/received-requests">Borrowing Requests</Link>}
+                {user && isOwner && <Link to="/received-requests">Incoming Requests</Link>}
                 
   
                 {user && <Link to="/sent-requests">My Borrowing Requests</Link> }
